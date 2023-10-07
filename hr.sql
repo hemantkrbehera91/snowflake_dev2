35a761d75725e1673c351d416798bc8b9a36c54d
hemantkrbehera <hemant.krbehera91@gmail.com>
2023-10-07 20:02:03 +0530

    git versioning tool

---
+++ hr.sql
@@ -0,0 +1,23 @@
+version control system is also known as software configuration management(scm) or source code manager(scm)
+
+
+developer-->write code-->files
+
+
+100 files developed
+client suggested some changes
+i changed some file source code to meet client requirement(V1)
+client again wanted some changes
+i changed some file source code to meet client requirement(v2)
+client again wanted some changes
+
+
+https://github.com/hemantkrbehera91/snowflake_Dev.git
+
+
+clone
+add
+commit
+push
+
+
